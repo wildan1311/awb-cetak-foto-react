05dd5541349f574c9fc35ebb72ea3c061988d900
wildan1311 <wmaaf1311@gmail.com>
2024-06-25 21:31:28 +0700

    setting tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
       extend: {},
+      container: {
+        center: true,
+        padding: '2rem',
+        screens: {
+          '2xl': '1400px',
+        },
+      },
     },
     plugins: [],
   }
